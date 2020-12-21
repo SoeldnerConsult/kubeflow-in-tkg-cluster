c829327e01d97347ca82bfc5c164f7cfa22dff00
Kevin Kekule <k.kekule@web.de>
2020-12-21 12:35:59 +0100

    put stuff into a function so all the code is not called, when sourced

--- kubeflow-installation.sh
+++ kubeflow-installation.sh
@@ -14,18 +14,17 @@ function check_for_set_var() {
 }
 
 val=$(check_for_set_var $storageclass)
-allset=$((allset+val))
+allset=$((allset + val))
 val=$(check_for_set_var $namespace)
-allset=$((allset+val))
+allset=$((allset + val))
 val=$(check_for_set_var $clustername)
-allset=$((allset+val))
+allset=$((allset + val))
 val=$(check_for_set_var $supervisor_ip)
-allset=$((allset+val))
+allset=$((allset + val))
 val=$(check_for_set_var $username_and_domain)
-allset=$((allset+val))
+allset=$((allset + val))
 
-
-if [[ $allset -ne 0 ]];then
+if [[ $allset -ne 0 ]]; then
   echo "you need to set and export the following variables:"
   echo "export storageclass=??"
   echo "export namespace=??"
@@ -73,63 +72,65 @@ source "${DIR}/${subdir}/open-up-services.sh"
 source "${DIR}/${subdir}/post-patch-argo.sh"
 source "${DIR}/${subdir}/ldap_connector_configuration_script.sh"
 
-#prepare some environment variables and login
-prepare_tkg_cluster_creation $storageclass $vmclass_cp $vmclass_worker \
-$vm_image $namespace $clustername $supervisor_ip $username_and_domain
+function install() {
+  #prepare some environment variables and login
+  prepare_tkg_cluster_creation $storageclass $vmclass_cp $vmclass_worker \
+  $vm_image $namespace $clustername $supervisor_ip $username_and_domain
 
-#if succesfully logged in, proceed with cluster creation
-create_cluster $supervisor_ip $clustername $namespace $vm_image \
-$control_machine_count $worker_machine_count
+  #if succesfully logged in, proceed with cluster creation
+  create_cluster $supervisor_ip $clustername $namespace $vm_image \
+  $control_machine_count $worker_machine_count
 
-#if cluster was successfully installed, patch the api server
-append_api_server_flags $supervisor_ip $namespace $clustername $username_and_domain
+  #if cluster was successfully installed, patch the api server
+  append_api_server_flags $supervisor_ip $namespace $clustername $username_and_domain
 
-#download kubeflow files
-download_and_unpack_kubeflow
-create_local_kubeflow_kustomize_build
+  #download kubeflow files
+  download_and_unpack_kubeflow
+  create_local_kubeflow_kustomize_build
 
-#when api server patch is finished, proceed with kubeflow preinstall patching
-create_psp_rolebinding_patches
-patch_knative_deployment_errors
+  #when api server patch is finished, proceed with kubeflow preinstall patching
+  create_psp_rolebinding_patches
+  patch_knative_deployment_errors
 
-#install kubeflow and wait for it to be ready
-apply_kubeflow_kustomize
-wait_for_kubeflowinstall
+  #install kubeflow and wait for it to be ready
+  apply_kubeflow_kustomize
+  wait_for_kubeflowinstall
 
-#well, patch argo in kubernetes
-post_patch_argo_from_docker_to_pns
+  #well, patch argo in kubernetes
+  post_patch_argo_from_docker_to_pns
 
-#allow access to kubeflow
-make_kubeflow_accessible_via_https_from_outside
+  #allow access to kubeflow
+  make_kubeflow_accessible_via_https_from_outside
 
-#BEFORE you proceed with this step I !!!highly!!! recommend
-#Reading this blogpost:
-#https://cloudadvisors.net/2020/09/23/ldap-active-directory-with-kubeflow-within-tkg/
-#for getting a clue, what we're trying to do..
-#we do NOT want to follow the whole script and import the
-#whole LDAP / AD Directory, but at least you'll understand, which
-#information are necessary in the following variables!
+  #BEFORE you proceed with this step I !!!highly!!! recommend
+  #Reading this blogpost:
+  #https://cloudadvisors.net/2020/09/23/ldap-active-directory-with-kubeflow-within-tkg/
+  #for getting a clue, what we're trying to do..
+  #we do NOT want to follow the whole script and import the
+  #whole LDAP / AD Directory, but at least you'll understand, which
+  #information are necessary in the following variables!
 
-#if you missconfigure this step, you need to fix it
-#manually in the corresponding yaml file with
-#kubectl edit configmap dex
-host="DC1.vdi.sclabs.net"
-bindDN="cn=kates,ou=UEMUsers,dc=vdi,dc=sclabs,dc=net"
-#caution with the password, as for example an exclamation mark (!) may cause trouble
-#if this is the case, it may be better to put in a dummy
-#password and after that, to edit the configmap manually
-#  with insertion of the correct password in your favorite editor i.e. vim
-bindPW="<pwOfKatesUser>"
-username_prompt="vdi user + domain, e.g. kevin@vdi.sclabs.net"
-baseDN="ou=UEMUsers,dc=vdi,dc=sclabs,dc=net"
-username_attribute="userPrincipalName"
-id_attribute="sAMAccountName"
-email_attribute="userPrincipalName"
-name_attribute="displayName"
+  #if you missconfigure this step, you need to fix it
+  #manually in the corresponding yaml file with
+  #kubectl edit configmap dex
+  host="DC1.vdi.sclabs.net"
+  bindDN="cn=kates,ou=UEMUsers,dc=vdi,dc=sclabs,dc=net"
+  #caution with the password, as for example an exclamation mark (!) may cause trouble
+  #if this is the case, it may be better to put in a dummy
+  #password and after that, to edit the configmap manually
+  #  with insertion of the correct password in your favorite editor i.e. vim
+  bindPW="<pwOfKatesUser>"
+  username_prompt="vdi user + domain, e.g. kevin@vdi.sclabs.net"
+  baseDN="ou=UEMUsers,dc=vdi,dc=sclabs,dc=net"
+  username_attribute="userPrincipalName"
+  id_attribute="sAMAccountName"
+  email_attribute="userPrincipalName"
+  name_attribute="displayName"
 
-configure_dex_ldap_connector $host $bindDN $bindPW "$username_prompt" \
-$baseDN $username_attribute $id_attribute $email_attribute $name_attribute
+  configure_dex_ldap_connector $host $bindDN $bindPW "$username_prompt" \
+  $baseDN $username_attribute $id_attribute $email_attribute $name_attribute
 
-#if you need to change pw:
-kubectl edit -n auth cm dex
-kubectl rollout restart deployment dex -n auth
+  #if you need to change pw:
+  kubectl edit -n auth cm dex
+  kubectl rollout restart deployment dex -n auth
+}
